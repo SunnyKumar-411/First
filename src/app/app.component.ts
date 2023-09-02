6e70d8d1a09a760157a6796377400948526fcd41
SunnyKumar-411 <you@example.com>
2023-09-02 13:08:44 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: '<h1>Sunny Kumar</h1> <h1>Bca</h1> <h1> Myself Sunny Kumar I am java faculty in Anudip Foundation</h1>'
 })
 export class AppComponent {
   title = 'First';
